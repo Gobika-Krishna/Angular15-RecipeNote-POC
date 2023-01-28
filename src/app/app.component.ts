0f02ca64526abf75aa15da42fdd660ab88bd96df
Gobika <gobikakrishna@gmail.com>
2023-01-28 15:35:50 +0530

    Data Binding with Input, Output, viewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shoppingApp';
+  feature = 'recipe';
+  onNavigate(feature) {
+    this.feature = feature
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe 1', 'Test desc',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fimage%2F2017%2F05%2Fmain%2Fgrilled-chicken-drumsticks-bourbon-cherry-bbq-sauce-1707p126.jpg%3Fitok%3Dus5SCOVu&w=450&h=223&c=sc&poi=face&q=60'),
+    new Recipe('A test Recipe 2', 'Test desc',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fimage%2F2017%2F05%2Fmain%2Fgrilled-chicken-drumsticks-bourbon-cherry-bbq-sauce-1707p126.jpg%3Fitok%3Dus5SCOVu&w=450&h=223&c=sc&poi=face&q=60')
+  ];
+
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
+  constructor() { }
+  ngOnInit() { }
+  onSelectItem(recipe: Recipe) {
+    this.selectedRecipe.emit(recipe);
+  }
 
 }
