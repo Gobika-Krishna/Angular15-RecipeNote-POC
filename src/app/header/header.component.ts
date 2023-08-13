f402690c45800318efa7845a924b262a19f7aa26
Gobika K <gobikakrishna@gmail.com>
2023-08-13 19:43:19 +0200

    Added header and recipe items style

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,7 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   constructor(
